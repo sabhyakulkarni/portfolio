d839d3f24d98249c2447d3319649bc29d3279608
sabhyakulkarni <sabhyakulkarni18@gmail.com>
2025-11-21 06:20:52 +0530

    Fix import case for Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/Header/header";
+import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Skills from "./components/skills/Skills";
@@ -20,6 +20,7 @@ const App = () => {
             <Qualification />
             <Work />
             <Footer />
+            <Header />
         </main>
         </>
     )
